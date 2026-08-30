da96626eaa2dde846023305c17f21f4e70678b25
valeri-Tauqueer-dot <mdtauqueerhussain123@gmail.com>
2026-08-30 16:05:22 +0530

    Add AlphaBrain class with initialization

--- alpha_brain.py
+++ alpha_brain.py
@@ -1,1 +1,11 @@
+from vale_connector import VALEConnector
+from vale_brain_interface import VALEBrainInterface
 
+
+class AlphaBrain(VALEBrainInterface):
+
+    def __init__(self, connector):
+        super().__init__(
+            brain_name="ALPHA",
+            connector=connector
+        )
