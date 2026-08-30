7e7659900ebdd69d1742bc23eac00ee0cb64f397
valeri-Tauqueer-dot <mdtauqueerhussain123@gmail.com>
2026-08-30 16:05:48 +0530

    Update legend_brain.py

--- legend_brain.py
+++ legend_brain.py
@@ -1,1 +1,11 @@
+from vale_connector import VALEConnector
+from vale_brain_interface import VALEBrainInterface
 
+
+class LegendBrain(VALEBrainInterface):
+
+    def __init__(self, connector):
+        super().__init__(
+            brain_name="LEGEND",
+            connector=connector
+        )
