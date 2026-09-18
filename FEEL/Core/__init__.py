bc4d822128067bbb15878d6e40567a741fb5b599
Vale-Tauqueer-dot <mdtauqueerhussain123@gmail.com>
2026-09-18 21:28:40 +0530

    Update __init__.py

--- FEEL/Core/__init__.py
+++ FEEL/Core/__init__.py
@@ -1,1 +1,83 @@
+"""
+VALE FEELING Brain
+Core Package
 
+The core package contains the shared data models, state management,
+and subsystem contracts used throughout the FEELING Brain.
+"""
+
+from .models import (
+    EpistemicType,
+    ConfidenceLevel,
+    EvidenceStrength,
+    EmotionalSignalType,
+    IdeaStatus,
+    EvidenceReference,
+    EpistemicClaim,
+    EmotionalSignal,
+    HumanContext,
+    PsychologicalSignal,
+    PsychologicalState,
+    ImaginationCandidate,
+    Idea,
+    Perspective,
+    CommunicationProfile,
+    FeelingAssessment,
+)
+
+from .state import (
+    FeelingState,
+    FeelingStateSnapshot,
+)
+
+from .contracts import (
+    EvidenceItem,
+    CognitiveContext,
+    EngineResult,
+    PsychologicalResult,
+    HumanUnderstandingResult,
+    CreativityResult,
+    MCVLChallenge,
+    MCVLChallengeResult,
+    InterBrainContext,
+    FeelingEngineRequest,
+    FeelingEngineResponse,
+)
+
+
+__all__ = [
+    # Models
+    "EpistemicType",
+    "ConfidenceLevel",
+    "EvidenceStrength",
+    "EmotionalSignalType",
+    "IdeaStatus",
+    "EvidenceReference",
+    "EpistemicClaim",
+    "EmotionalSignal",
+    "HumanContext",
+    "PsychologicalSignal",
+    "PsychologicalState",
+    "ImaginationCandidate",
+    "Idea",
+    "Perspective",
+    "CommunicationProfile",
+    "FeelingAssessment",
+
+    # State
+    "FeelingState",
+    "FeelingStateSnapshot",
+
+    # Contracts
+    "EvidenceItem",
+    "CognitiveContext",
+    "EngineResult",
+    "PsychologicalResult",
+    "HumanUnderstandingResult",
+    "CreativityResult",
+    "MCVLChallenge",
+    "MCVLChallengeResult",
+    "InterBrainContext",
+    "FeelingEngineRequest",
+    "FeelingEngineResponse",
+]
