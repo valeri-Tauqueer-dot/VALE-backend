90f1d0e2cf35831e1b3ecaf5701ea82722f1918e
Vale-Tauqueer-dot <mdtauqueerhussain123@gmail.com>
2026-09-20 15:43:26 +0530

    Create __init__.py

---
+++ HEROIC/Intent/__init__.py
@@ -0,0 +1,12 @@
+"""
+HEROIC INTENT PACKAGE
+
+Foundational intent representations for the HEROIC Brain.
+"""
+
+from .intent_state import HeroicIntentState, IntentType
+
+__all__ = [
+    "HeroicIntentState",
+    "IntentType",
+]
