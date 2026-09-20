4948358c8855c41a64c0d575ef0b18f1cd974e1f
Vale-Tauqueer-dot <mdtauqueerhussain123@gmail.com>
2026-09-20 15:49:28 +0530

    Create __init__.py

---
+++ HEROIC/goals/__init__.py
@@ -0,0 +1,12 @@
+"""
+HEROIC GOALS PACKAGE
+
+Foundational goal representations for the HEROIC Brain.
+"""
+
+from .goal_state import HeroicGoalState, GoalStatus
+
+__all__ = [
+    "HeroicGoalState",
+    "GoalStatus",
+]
