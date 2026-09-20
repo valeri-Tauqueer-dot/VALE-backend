cc402120e89a754437eafbbecaa2802245e59dfc
Vale-Tauqueer-dot <mdtauqueerhussain123@gmail.com>
2026-09-20 15:52:54 +0530

    Create __init__.py

---
+++ HEROIC/tasks/__init__.py
@@ -0,0 +1,13 @@
+"""
+HEROIC TASKS PACKAGE
+
+Foundational task representations for the HEROIC Brain.
+"""
+
+from .task_state import HeroicTaskState, TaskStatus, TaskType
+
+__all__ = [
+    "HeroicTaskState",
+    "TaskStatus",
+    "TaskType",
+]
