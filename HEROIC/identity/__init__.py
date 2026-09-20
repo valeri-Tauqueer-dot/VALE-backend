527d9a4749f8e95a38d42a07a9f94ce6d0691176
Vale-Tauqueer-dot <mdtauqueerhussain123@gmail.com>
2026-09-20 15:36:59 +0530

    Create __init__.py

---
+++ HEROIC/identity/__init__.py
@@ -0,0 +1,14 @@
+"""
+HEROIC IDENTITY PACKAGE
+
+Defines the identity of a HEROIC mission.
+
+Mission identity provides stable identification and basic
+traceability for objectives moving through the HEROIC lifecycle.
+"""
+
+from .mission_identity import HeroicMissionIdentity
+
+__all__ = [
+    "HeroicMissionIdentity",
+]
